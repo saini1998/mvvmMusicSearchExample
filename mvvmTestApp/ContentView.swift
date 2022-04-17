e572916c1b53981303845ff021e1cb0e83ebbc72
saini1998 <sainiaaryaman1998@gmail.com>
2022-04-17 20:21:53 +0100

    Initial Commit

---
+++ mvvmTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mvvmTestApp
+//
+//  Created by Aaryaman Saini on 17/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mvvmTestApp/mvvmTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmTestAppApp.swift
+//  mvvmTestApp
+//
+//  Created by Aaryaman Saini on 17/04/22.
+//
+
+import SwiftUI
+
+@main
+struct mvvmTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
